650837f130e60ae5c026d7b128a8165673368b7c
razuahmedjoy <rjjoybzs420@gmail.com>
2022-04-26 00:31:20 +0600

    fixed link

--- aapbookstore/urls.py
+++ aapbookstore/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('bookstore.urls')),
-    path('secreta/aapadminaap/', admin.site.urls),
+    path('secret/aapadminaap/', admin.site.urls),
 
   
 ]
