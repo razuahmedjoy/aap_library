e94ae0ae542510851ad0f16b8363412115e607dd
razuahmedjoy <rjjoybzs420@gmail.com>
2022-04-26 00:17:17 +0600

    changed admin linl

--- aapbookstore/urls.py
+++ aapbookstore/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('bookstore.urls')),
-    path('secret/aapadminaap/', admin.site.urls),
+    path('secreta/aapadminaap/', admin.site.urls),
 
   
 ]
