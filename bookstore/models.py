2d8aa58f1e237383cacca23cf0c9216c5ef25e97
Ariful islam <96423861+arif010@users.noreply.github.com>
2022-05-23 20:33:06 +0600

    Update models.py
    remove unwanted import

--- bookstore/models.py
+++ bookstore/models.py
@@ -2,7 +2,6 @@ from operator import mod
 from pickle import TRUE
 from pyexpat import model
 from random import choices
-from tkinter.tix import Tree
 from django.db import models
 from django.contrib.auth.models import User
 import os
